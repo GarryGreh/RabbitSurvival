173aaa776c4a8901646a2fc25f607c22ab843fa1
GarryGreh <garik.greh@yandex.ru>
2024-06-28 19:19:05 +0700

    123

--- RabbitSurvival/Assets/_Scripts/PlayerControll/ThirdPersonalInput.cs
+++ RabbitSurvival/Assets/_Scripts/PlayerControll/ThirdPersonalInput.cs
@@ -13,36 +13,49 @@ public class ThirdPersonalInput : MonoBehaviour
     private void Start()
     {
         tpc = GetComponent<ThirdPersonalController>();
-       // tpAttack = GetComponent<ThirdPersonalAttack>();
+
+        // если загружено с ПК, то спрятать джойстик
+        if(Application.isEditor)
+        {
+            joystick.gameObject.SetActive(false);
+        }
+        // иначе, если мобильный, то включить джойстик
+        else if(Application.isMobilePlatform)
+        {
+            joystick.gameObject.SetActive(true);
+        }
+        
+        // tpAttack = GetComponent<ThirdPersonalAttack>();
     }
     private void Update()
     {
-        tpc.Movement(joystick.Horizontal, joystick.Vertical);
-        //if (Application.isEditor)
-        //{
-        //    tpc.Movement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //    if (Input.GetKey(KeyCode.LeftShift))
-        //    {
-        //        tpc.Run(5.0f);
-        //    }
-        //    else if (Input.GetKeyUp(KeyCode.LeftShift))
-        //    {
-        //        tpc.Run(1.5f);
-        //    }
-        //    //if (Input.GetMouseButtonDown(0))
-        //    //{
-        //    //    tpAttack.AttackPaw();
-        //    //}
-        //    //if (Input.GetMouseButtonUp(1))
-        //    //{
-        //    //    tpAttack.AttackRoar();
-        //    //}
-        //}
-        //else
-        //{
-        //    // мобильное упроавление
-        //   // tpc.Movement(joystickPers.Horizontal, joystickPers.Vertical);            
-        //}
+       // tpc.Movement(joystick.Horizontal, joystick.Vertical);
+        if (Application.isEditor)
+        {
+            tpc.Movement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));            
+            //if (Input.GetKey(KeyCode.LeftShift))
+            //{
+            //    tpc.Run(5.0f);
+            //}
+            //else if (Input.GetKeyUp(KeyCode.LeftShift))
+            //{
+            //    tpc.Run(1.5f);
+            //}
+            //if (Input.GetMouseButtonDown(0))
+            //{
+            //    tpAttack.AttackPaw();
+            //}
+            //if (Input.GetMouseButtonUp(1))
+            //{
+            //    tpAttack.AttackRoar();
+            //}
+        }
+        else
+        {
+            // мобильное упроавление
+            //tpc.Movement(joystickPers.Horizontal, joystickPers.Vertical);
+            tpc.Movement(joystick.Horizontal, joystick.Vertical);
+        }
     }
     public void Run(bool _isRun)
     {
